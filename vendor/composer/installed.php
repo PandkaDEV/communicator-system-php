80812b3b41b1963585974cee291a975bc1661fea
Pieszku <46606720+Piechuuu@users.noreply.github.com>
2023-01-07 12:06:58 +0100

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nats/nats',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'eec27d45302131d3e25d3368f0d53cf54641ad36',
+        'type' => 'l',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nats/nats' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'eec27d45302131d3e25d3368f0d53cf54641ad36',
+            'type' => 'l',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
